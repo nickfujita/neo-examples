277927127eea376d500c3fd53052a8db34bf98f7
Nick Fujita <nickfujita@gmail.com>
2018-02-15 20:18:38 +0900

    Add callerValidation contract (#6)
    * Add callerValidation contract
    * Fix build test comments in other contracts

--- intro/01-helloWorld.py
+++ intro/01-helloWorld.py
@@ -5,7 +5,7 @@
 # testinvoke <your_contract_hash>
 #
 # BUILD TEST
-# build smartContracts/01-helloWorld.avm test "" 01 False False
+# build smartContracts/01-helloWorld.py test "" 01 False False
 
 def Main():
 
