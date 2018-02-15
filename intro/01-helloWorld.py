cced5dfd5a6b42079b306f824e833880e60960cd
nicolasfujita <nickfujita@gmail.com>
2018-02-15 19:57:43 +0900

    Add comments to sample contracts for calls to import, testinvoke, build test

--- intro/01-helloWorld.py
+++ intro/01-helloWorld.py
@@ -1,3 +1,12 @@
+# DEPLOY
+# import contract smartContracts/01-helloWorld.avm "" 01 False False
+#
+# INVOKE
+# testinvoke <your_contract_hash>
+#
+# BUILD TEST
+# build smartContracts/01-helloWorld.avm test "" 01 False False
+
 def Main():
 
     print("hello world");
