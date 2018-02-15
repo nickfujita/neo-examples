8105fadd4eaf1b47803c635527512d9e9b51d1c3
nicolasfujita <nickfujita@gmail.com>
2018-02-15 19:48:22 +0900

    updates with comments

--- intro/03-userAuth.py
+++ intro/03-userAuth.py
@@ -1,11 +1,14 @@
 from boa.blockchain.vm.Neo.Runtime import CheckWitness
 
+# A simple function to demonstrate the usage of CheckWitness
+# This contract will validate the provided input key/hash matches that of callerHash
+
 def Main(callerHash):
 
     isMatch = CheckWitness(callerHash);
 
     if isMatch:
-        print("Caller hash matches provided hash!")
+        print("Caller hash/key matches provided hash/key!")
         return True
 
     print("IMPOSTER!!!")
