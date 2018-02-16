90f74b6b2a52d717cb1b743c78d3af6aabfdcc82
nicolasfujita <nickfujita@gmail.com>
2018-02-16 17:03:30 +0900

    reordered comments. added concat example

--- intro/02-calculator.py
+++ intro/02-calculator.py
@@ -1,11 +1,12 @@
+
+# BUILD TEST
+# build smartContracts/02-calculator.py test 070202 02 False False plus 1 1
+#
 # IMPORT
 # import contract smartContracts/02-calculator.avm 070202 02 False False
 #
 # INVOKE
 # testinvoke <your_contract_hash> plus 1 1
-#
-# BUILD TEST
-# build smartContracts/02-calculator.py test 070202 02 False False plus 1 1
 
 def Main(operation, foo, bar):
 
