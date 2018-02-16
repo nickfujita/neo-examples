32458bc624bf782951cb0b0408dfdb77d68a200b
Nick Fujita <nickfujita@gmail.com>
2018-02-16 17:04:47 +0900

    Added concat example (#8)
    - Added concat example
    - Reordered comments

--- intro/01-helloWorld.py
+++ intro/01-helloWorld.py
@@ -1,11 +1,12 @@
+
+# BUILD TEST
+# build smartContracts/01-helloWorld.py test "" 01 False False
+#
 # IMPORT
 # import contract smartContracts/01-helloWorld.avm "" 01 False False
 #
 # INVOKE
 # testinvoke <your_contract_hash>
-#
-# BUILD TEST
-# build smartContracts/01-helloWorld.py test "" 01 False False
 
 def Main():
 
